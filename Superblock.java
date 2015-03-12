ca7695a30da87c74c6ae80bf64dd69551093c88b
Carl Martinez <carlmar89@live.com>
2015-03-12 00:39:00 -0700

    initial files

---
+++ Superblock.java
@@ -0,0 +1,9 @@
+import java.lang.Exception;
+import java.util.*;
+
+public class Superblock
+{
+	public int totalBlocks; // the number of disk blocks
+	public int totalInodes; // the number of inodes
+	public int freeList;    // the block number of the free list's head
+}
