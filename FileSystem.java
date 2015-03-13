b5179b1aa9244e300fafd4f8029596c0ecf4eefa
Carl Martinez <carlmar89@live.com>
2015-03-13 01:33:04 -0700

    FileSystem outline

--- FileSystem.java
+++ FileSystem.java
@@ -3,8 +3,40 @@ import java.util.*;
 
 public class FileSystem
 {
+	private Directory dir;
+
 	public FileSystem()
 	{
-		
+		dir = new Directory();
+	}
+
+	public int read(int fd, byte buffer[])
+	{
+
+	}
+
+	public int write(int fd, byte buffer[])
+	{
+
+	}
+
+	public int seek(int fd, int offset, int whence)
+	{
+
+	}
+
+	public int close(int fd)
+	{
+
+	}
+
+	public int delete(String fileName)
+	{
+
+	}
+
+	public int fsize(int fd)
+	{
+
 	}
 }
