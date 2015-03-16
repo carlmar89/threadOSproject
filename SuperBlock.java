6fa95346caff14bed7042f1e5d1a1c5e91a29346
Terry Rogers <terryrogers@outlook.com>
2015-03-15 20:04:18 -0700

    SuperBlock finished.

--- SuperBlock.java
+++ SuperBlock.java
@@ -8,23 +8,28 @@ public class SuperBlock
 	public int totalBlocks; // the number of disk blocks
 	public int totalInodes; // the number of inodes
 	public int freeList;    // the block number of the free list's head
+	public int lastFreeBlock;
 
-	public SuperBlock(int diskSize)
+	public SuperBlock()
 	{
 		byte[] superBlock = new byte[Disk.blockSize];
+
 		SysLib.rawread(0, superBlock);
+
 		totalBlocks = SysLib.bytes2int(superBlock, 0);
 		totalInodes = SysLib.bytes2int(superBlock, 4);
 		freeList = SysLib.bytes2int(superBlock, 8);
+		lastFreeBlock = SysLib.bytes2int(superBlock, 12);
 
-		if (totalBlock == diskSize && totalInodes > 0 && freeList >= 2)
+		// The freeList has to be 2 or greater since the first block (index 0)
+		// is the SuperBlock, and the the second block (index 1) contains
+		// information about Inodes.
+		if(totalBlocks != Kernel.NUM_BLOCKS || totalInodes <= 0 || 
+			freeList < 2 || freeList >= Kernel.NUM_BLOCKS && 
+			lastFreeBlock < 2 || lastFreeBlock >= totalBlocks)
 		{
-			//valid disk contents
-			return;
-		}
-		else
-		{
-			totalBlock = diskSize;
+			totalBlocks = Kernel.NUM_BLOCKS;
+			lastFreeBlock = totalBlocks - 1;
 
 			SysLib.format(DEFAULT_INODE_BLOCKS);
 		}
@@ -32,16 +37,75 @@ public class SuperBlock
 
 	public sync()
 	{
+		byte[] buffer = new byte[Disk.blockSize];
 
+		// Write the totalBlocks, totalInodes, and freeList.
+		SysLib.int2bytes(totalBlocks, buffer, 0);
+		SysLib.int2bytes(totalInodes, buffer, 4);
+		SysLib.int2bytes(freeList, buffer, 8);
+		SysLib.int2bytes(lastFreeBlock, buffer, 12);
+
+		// Write the block back to disk.
+		SysLib.rawwrite(0, buffer);
 	}
 
-	public getFreeBlock()
+	public int getFreeBlock()
 	{
-		//dequeue the top block from the free list
+		// Store the current free block temporarily.
+		int currentFreeBlock = freeList;
+
+		if(currentFreeBlock != -1)
+		{
+			byte[] buffer = new byte[Disk.blockSize];
+
+			// Read the current free block into the buffer.
+			SysLib.rawread(currentFreeBlock, buffer);
+
+			// Update the pointer to the next free block.
+			freeList = SysLib.bytes2int(buffer, 0);
+
+			if(freeList == -1)
+				lastFreeBlock = -1;
+
+			// Update the current free block's pointer.
+			SysLib.int2bytes(-1, buffer, 0);
+
+			// Write the current free block back to the disk.
+			SysLib.rawwrite(currentFreeBlock, buffer);
+		}
+
+		return currentFreeBlock;
 	}
 
-	public returnBlock(int blockNumber)
+	public void returnBlock(int blockNumber)
 	{
-		//enqueue a given block to the end of the free list
+		int firstFreeBlock = DEFAULT_INODE_BLOCKS / 
+			(Disk.blockSize / Inode.iNodeSize) + 1;
+
+		if(blockNumber >= firstFreeBlock && blockNumber < totalBlocks)
+		{
+			byte[] buffer = new byte[Disk.blockSize];
+
+			// Read the current last free block into the buffer.
+			SysLib.rawread(lastFreeBlock, buffer);
+
+			// Update the current last free block's pointer.
+			SysLib.int2bytes(blockNumber, buffer, 0);
+
+			// Write the current last block back to disk.
+			SysLib.rawwrite(lastFreeBlock, buffer);
+
+			// Update the SuperBlock's pointer.
+			lastFreeBlock = blockNumber;
+
+			// Read the new last free block into the buffer.
+			SysLib.rawread(lastFreeBlock, buffer);
+
+			// Update the new last free block's pointer.
+			SysLib.int2bytes(-1, buffer, 0);
+
+			// Write the new last free block back to disk.
+			SysLib.rawwrite(lastFreeBlock, buffer);
+		}
 	}
 }
